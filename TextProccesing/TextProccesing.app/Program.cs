bc276f1fc5dd843be9c14d37bf4bef884f0aca24
cmandea <146120390+cmandea@users.noreply.github.com>
2023-11-11 17:54:41 +0200

    add Insert substring into string

--- TextProccesing/TextProccesing.app/Program.cs
+++ TextProccesing/TextProccesing.app/Program.cs
@@ -6,28 +6,34 @@ namespace TextProccesing.app
     {
         static void Main(string[] args)
         {
+            //Sirul Original
             string input = "Random String Creator Online is easy to use tool to generate String based on options such as number of rows of string";
 
             Console.WriteLine($"Original Text is:\n{input}");
 
-            TextProcessor processorReplaceRule = new TextProcessor(
-                new ReplaceStringProcessingRule("String", "Text"));
-
+            //Inlocuire in sir
+            TextProcessor processorReplaceRule = new TextProcessor(new ReplaceStringProcessingRule("String", "Text"));
             string result = processorReplaceRule.ApplyAllProcessingRule(input);
-            Console.WriteLine($"Replace 'String' with 'Text':\n{result}");
+            Console.WriteLine($"Replace 'String' with 'Text':\n{result}\n");
             
-            TextProcessor processorRemoveRule = new TextProcessor(
-                          new RemoveStringProcessingRule("Online"));
+            //Eliminare din sir
+            TextProcessor processorRemoveRule = new TextProcessor(new RemoveStringProcessingRule("Online"));
             result = processorRemoveRule.ApplyAllProcessingRule(input);
-            Console.WriteLine($"Remove 'Online':\n{result}");
+            Console.WriteLine($"Remove 'Online':\n{result}\n");
 
-            TextProcessor processorAllRules = new TextProcessor(
-               new ReplaceStringProcessingRule("String", "Text"),
-               new RemoveStringProcessingRule("Online"));
+            //Inserare in sir
+            TextProcessor processorInsertRule = new TextProcessor(new InsertStringProcessingRule("XXxXXxX",11));
+            result = processorInsertRule.ApplyAllProcessingRule(input);
+            Console.WriteLine($"Insert string 'XXxXXxX':\n{result} \n");
+
+            //Toate
+            TextProcessor processorAllRules = new TextProcessor(new ReplaceStringProcessingRule("String", "Text"),
+                                                                new RemoveStringProcessingRule("Online"),
+                                                                new InsertStringProcessingRule("XXxXXxX",100)
+                                                               );
 
              result = processorAllRules.ApplyAllProcessingRule(input);
-            
-            Console.WriteLine($"Replace 'String' with 'Text' and Remove 'Online':\n{result}");
+             Console.WriteLine($"Replace 'String' with 'Text' and Remove 'Online' and Insert 'XXxXXxX' from position 100:\n{result}");
         }
     }
 }  
---
+++ TextProccesing/TextProccesing.lib/InsertStringProcessingRule.cs
@@ -0,0 +1,42 @@
+﻿namespace TextProccesing.lib
+{
+    public class InsertStringProcessingRule : ProcessingRule
+        //pentru un sir dat sa se insereze un alt sir la o anumita pozitie
+    {
+        public InsertStringProcessingRule(string textInserted, int index)
+        { //am nevoie de pozitia(Index) de unde se insereaza noul sir(TextInserted)  si de sirul care se insereaza
+            if (textInserted is null) 
+            {
+            throw new ArgumentNullException(nameof(textInserted));
+            }
+            if (index<0) 
+            {
+                throw new ArgumentOutOfRangeException( nameof(index));
+            }
+            
+            TextInserted = textInserted;
+            Index = index;
+        } 
+        public string TextInserted { get; }
+        public int Index { get; }
+
+        public override string Processing(string input)
+        {
+            //trebuie sa testez si input pt null
+            if (input is null)
+            {
+            throw new ArgumentNullException("Sirul in care se insereaza nu poate fi null.",nameof(input));
+            }
+            //mai trebuie sa validez daca idexul este >lungimea input-ului
+            if (Index > input.Length) 
+            {
+                throw new IndexOutOfRangeException(nameof(Index));
+            }
+            string result = input.Insert(Index, TextInserted);
+            return result;
+
+        }
+    }
+
+  
+}
